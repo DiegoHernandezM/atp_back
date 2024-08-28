f699a426ccf5500dfa54dff70e754c2d48fba2f2
Diego Hernandez <dhernandezm@iMac-de-Diego.local>
2024-08-28 11:55:42 -0600

    seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
