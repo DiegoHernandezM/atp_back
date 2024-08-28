9dc5edbe1455de58f5db24ac3a8ba3fba3bfa3e9
Diego Hernandez <dhernandezm@iMac-de-Diego.local>
2024-08-28 11:52:37 -0600

    seeders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator  $url)
     {
-        if (env('APP_ENV') == 'production') {
+        if (env('APP_ENV') == 'development') {
             $url->forceScheme('https');
         }
     }
